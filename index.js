901abe1200ec44ad0e453b5ce179fd52bda526d0
Igor <ivoivoivo@mail.ru>
2022-06-22 17:15:45 +0300

    page count calculation

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 "use strict";
+function amountOfPages(summary) {
+    let str = "";
+    let value = 0;
+    for (let i = 1; i <= summary; i++) {
+        value += 1;
+        str += value;
+        if (str.length == summary) {
+            return value
+        }
+    }
+    return value;
+}
