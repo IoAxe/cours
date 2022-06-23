17ceac50ac5602f044f0403ffc7d77b39286bc37
Igor <ivoivoivo@mail.ru>
2022-06-23 10:44:45 +0300

    recursion

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function amountOfPages(summary) {
     return value;
 }
 */
-
+/*
 function isPangram(string) {
     let set = new Set();
     let arr = string.split(' ').join('').split('');
@@ -26,6 +26,18 @@ function isPangram(string) {
         return false;
     }
 }
+*/
 
 
-
+// Вариант с циклом
+function deepCount(a) {
+    let count = a.length;
+    for (let i = 0; i < a.length; i++) if (Array.isArray(a[i])) count += deepCount(a[i]);
+    return count;
+}
+/*
+// Вариант с методом reduce
+function deepCount(a) {
+    return a.reduce((s, e) => s + (Array.isArray(e) ? deepCount(e) : 0), a.length);
+}
+*/
